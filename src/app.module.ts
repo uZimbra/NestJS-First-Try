9fc5cc1ae6f7f515d5be32e22d4a2575e0bb1c0a
Rodrigo A. Silva <rgbrasi2@gmail.com>
2021-07-06 21:25:43 -0300

    chore: .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://zimbra:<password>@cluster0.pioha.mongodb.net/test',
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.pioha.mongodb.net/test`,
     ),
     UsersModule,
   ],
